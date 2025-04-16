06c138da34211e2678989a571d6ac6a26a4eea0f
jeffreyz <jeffreyz@node0.jeffreyz-252458.wisr-pg0.wisc.cloudlab.us>
2025-04-16 17:01:52 -0600

    minor bug fixes

--- real_world.sh
+++ real_world.sh
@@ -1,7 +1,8 @@
 # [Num Runs] [Link Name] [Log Name]
 
+ssh $2 "mkdir logs"
 for i in $(seq 1 $1);
 do
     ./orca.sh 4 1111 $2 "${3}-run-${i}"
-    ./orca-off.sh 4 1111 $2 "${3}-run-${i}"
+    ./orca-off.sh 4 1111 $2 "${3}-off-run-${i}"
 done
